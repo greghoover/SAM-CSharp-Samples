25617553bd3caa99cf80eb448ca8d4b5d0f5268b
greg hoover <greg.hoover@sungardps.com>
2016-04-09 14:15:59 -0400

    - Added projects SAM.Spike.Library and SAM.Spike.WinformClient projects to start.

---
+++ SAM-CSharp-Samples/SAM.Spike.WinformClient/SpikeForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAM.Spike.WinformClient
+{
+	public partial class SpikeForm : Form
+	{
+		public SpikeForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
