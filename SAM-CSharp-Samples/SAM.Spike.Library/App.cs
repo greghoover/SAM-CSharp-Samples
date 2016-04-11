e8837da8ab5127b0236c15b3fa93601a6f4ad12d
greg hoover <greg.hoover@sungardps.com>
2016-04-11 00:50:29 -0400

    - Add App with rough translation of App from the JS sample.

---
+++ SAM-CSharp-Samples/SAM.Spike.Library/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Spike.Library
+{
+	public class App
+	{
+		public void Fred()
+		{
+			Func<Dispatch, State, string> app = (dispatch, state) =>
+			{
+				Action inc = () => dispatch.Request(new Accion { Type = "INC" });
+
+				return "HtmlTextDisplayingStateAndTriggeringIncOnClick";
+			};
+		}
+	}
+}
